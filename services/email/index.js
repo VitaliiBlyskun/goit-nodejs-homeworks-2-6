72179bb6d7f7817502bfce9e3b536e885503a3a8
Vitalii Blyskun <97634483+VitaliiBlyskun@users.noreply.github.com>
2023-03-20 11:45:31 +0200

    add fixes after mentors review

--- services/email/index.js
+++ services/email/index.js
@@ -1,9 +1,9 @@
-const sendEmail = require('./sendEmail');
-const verifyEmail = require('./verifyEmail');
-const resendVerifyEmail = require('./resendVerifyEmail');
+const sendEmail = require("./sendEmail");
+const verifyEmail = require("./verifyEmail");
+const resendVerifyEmail = require("./resendVerifyEmail");
 
 module.exports = {
-    sendEmail,
-    verifyEmail, 
-    resendVerifyEmail,
-}
+  sendEmail,
+  verifyEmail,
+  resendVerifyEmail,
+};
--- utils/index.js
+++ utils/index.js
@@ -7,3 +7,18 @@ module.exports = {
   controllerWrapper,
   handleMangooseError,
 };
+
+/////////////////////////////////////////
+
+// const HttpError = require("./HttpError");
+// const controllerWrapper = require("./controllerWrapper");
+// const handleMangooseError = require("./handleMangooseError");
+// const sendEmail = require('./sendEmail');
+
+// module.exports = {
+//   HttpError,
+//   controllerWrapper,
+//   handleMangooseError,
+//   sendEmail,
+// };
+
